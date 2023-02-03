5d14abc221990fc40b4a7f1b4fc834792464a757
Majo <majomrlhlavne@gmail.com>
2023-02-03 17:07:09 +0100

    Main Base

---
+++ Celestia site/main.js
@@ -0,0 +1,9 @@
+/* Nav bar responsivness */
+function navResposive() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
